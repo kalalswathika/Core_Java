3f26dd2554c5a47f9b6c703e84d75141cdac5588
kalalswathika <kalalswathika4@gmail.com>
2026-01-23 17:11:59 +0530

    Java Task

---
+++ src/com/custom/String.java
@@ -0,0 +1,7 @@
+package com.custom;
+
+public class String {
+	   public void show() {
+	        System.out.println("This is custom String class");
+	    }
+}
---
+++ src/com/test/Main.java
@@ -0,0 +1,12 @@
+package com.test;
+import com.custom.String; 
+public class Main {
+	    public static void main(java.lang.String[] args) {
+
+	        String s1 = new String();
+	        s1.show();
+
+	        java.lang.String s2 = "Hello Java";
+	        System.out.println(s2);
+	    }
+}
